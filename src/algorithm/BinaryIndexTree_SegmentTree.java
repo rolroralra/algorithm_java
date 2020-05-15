b00de0981d11a5d08bed298ec998154d228270b6
Shinyoung Kim <rolroralra@gmail.com>
2020-05-15 10:33:42 +0900

    Update BinaryIndexTree_SegmentTree.java

--- src/algorithm/BinaryIndexTree_SegmentTree.java
+++ src/algorithm/BinaryIndexTree_SegmentTree.java
@@ -2,48 +2,82 @@ package algorithm;
 
 public class BinaryIndexTree_SegmentTree {
 	public static int[] tree;
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		int[] arr = new int[]{4, 6, 3, 2, 4, 3, 1};
 		int N = arr.length;
-		
+
 		int size = 1;
+
 		for (; size < N; size <<= 1);
 		size = (size << 1) - 1;
-		
+
 		tree = new int[size];
 		
 		for (int i = 0; i < N; i++) {
 			update(0, N - 1, 0, i, arr[i]);
 		}
-		
-		System.out.println(sum2(0, N - 1, 0, 1, 4));
+
+		int left = 1;
+		int right = 4;
+
+		System.out.println(sum1(0, N - 1, 0, left, right));
+		System.out.println(sum2(0, N - 1, 0, left, right));
+
+
+		SegmentTree segmentTree = new SegmentTree(arr);
+		System.out.println(segmentTree.sum(left, right));
+
+		int totalCnt = 0;
+		int cnt = 0;
+		for (int i = 0; i < N; i++) {
+			for (int j = i; j < N; j++) {
+				int result1 = sum1(0, N-1, 0, i, j);
+				int result2 = sum2(0, N-1, 0, i, j);
+				int result3 = segmentTree.sum(i,j);
+				System.out.println("sum1(" + i +","+j +")= " + result1);
+				System.out.println("sum2(" + i +","+j +")= " + result2);
+				System.out.println("segmentTree.sum(" + i +","+j +")= " + result3);
+
+				totalCnt++;
+				if (result1 == result2 && result2 == result3) {
+					cnt++;
+				}
+			}
+		}
+		System.out.println();
+		System.out.println("Count Of Success: " + cnt + ", Total Count : " + totalCnt);
 		
 	}
-	
-	public static void update(int start, int end, int node, int index, int value) {
+
+	public static void update(int start, int end, int node, int index, int diffValue) {
+		// Escaping Condition : index is not in segment [start, end]
 		if (index < start || end < index) {
 			return;
 		}
-		
+
+		// index is in segment [start, end] ==> must be updated!
 		if (start <= index && index <= end) {
-			tree[node] += value;
+			tree[node] += diffValue;
 		}
-		
+
+		// When start == end, it must be leaf node in Segment Tree.
 		if (start != end) {
 			int mid = (start + end) >> 1;
 			int leftNode = (node << 1) + 1;
 			int rightNode = leftNode + 1;
-			update(start, mid, leftNode, index, value);
-			update(mid + 1, end, rightNode, index, value);
+			update(start, mid, leftNode, index, diffValue);
+			update(mid + 1, end, rightNode, index, diffValue);
 		}
 	}
-	
+
 	public static int sum1(int start, int end, int node, int left, int right) {
-		if (left < start || end < right) {
+		// [left, right] is not in [start, end]
+		if (left > end || right < start) {
 			return 0;
 		}
-		
-		if (left <= start && end <= right) {
+
+		// [left , right] > [start, end]
+		if (left <= start && right >= end) {
 			return tree[node];
 		}
 		
