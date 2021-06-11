57129ea1d469eb17b2e6ea9d64d27d90208166b1
Shinyoung Kim <rolroralra@gmail.com>
2021-06-11 14:57:22 +0900

    Update Eratosthenes.java

--- src/algorithm/Eratosthenes.java
+++ src/algorithm/Eratosthenes.java
@@ -1,9 +1,63 @@
 package algorithm;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.IntStream;
 
 public class Eratosthenes {
+	private final int MAX_NUMBER;
+	private final int SIEVE_LIST_SIZE;
+	private final List<Byte> SIEVE;
+
+	public Eratosthenes(int MAX_NUMBER) {
+		this.MAX_NUMBER = MAX_NUMBER;
+		SIEVE_LIST_SIZE = (MAX_NUMBER + Byte.SIZE - 1) / Byte.SIZE;
+		SIEVE = new ArrayList<>(SIEVE_LIST_SIZE);
+		IntStream.range(0, SIEVE_LIST_SIZE).forEach((i) -> SIEVE.add((byte) -1));
+
+		setComposite(0);
+		setComposite(1);
+
+		for (int i = 2; i * i <= MAX_NUMBER; i++) {
+			if (isComposite(i)) {
+				continue;
+			}
+
+			for (int j = i * i; j <= MAX_NUMBER; j += i) {
+				setComposite(j);
+			}
+		}
+	}
+
+	private void setPrime(int number) {
+		if (number < 0 || number > MAX_NUMBER) {
+			return;
+		}
+
+		SIEVE.set(number / Byte.SIZE, (byte) (SIEVE.get(number / Byte.SIZE) | (1 << (number % Byte.SIZE))));
+	}
+
+	private void setComposite(int number) {
+		if (number < 0 || number > MAX_NUMBER) {
+			return;
+		}
+
+		SIEVE.set(number / Byte.SIZE, (byte) (SIEVE.get(number / Byte.SIZE) & ~(1 << (number % Byte.SIZE))));
+	}
+
+	public boolean isPrime(int number) {
+		if (number < 0 || number > MAX_NUMBER) {
+			throw new RuntimeException(String.format("Number must be in range [%d, %d]", 0, MAX_NUMBER));
+		}
+
+		return (SIEVE.get(number / Byte.SIZE) & (1 << (number % Byte.SIZE))) != 0;
+	}
+
+	public boolean isComposite(int number) {
+		return !isPrime(number);
+	}
+
 
 	public static void main(String[] args) {
 		// ArrayList로 구현
@@ -15,9 +69,11 @@ public class Eratosthenes {
 		
 		// n <= 1 일 때 종료 
 		if(n <= 1) return;
-		
+
+		Eratosthenes eratosthenes = new Eratosthenes(n);
+
 		// n+1만큼 할당
-		primeList = new ArrayList<Boolean>(n+1);
+		primeList = new ArrayList<>(n+1);
 		// 0번째와 1번째를 소수 아님으로 처리 
 		primeList.add(false);
 		primeList.add(false);
@@ -38,12 +94,19 @@ public class Eratosthenes {
 				}*/
 			}
 		}
+
+
+
+
 		StringBuffer sb = new StringBuffer();
 		sb.append("{");
 		for (int i = 0; i <= n; i++){
-			if (primeList.get(i) == true){
-				sb.append(i);
-				sb.append(",");
+//			if (primeList.get(i) == true){
+//				sb.append(i);
+//				sb.append(",");
+//			}
+			if (eratosthenes.isPrime(i)) {
+				sb.append(i).append(",");
 			}
 		}
 		sb.setCharAt(sb.length() - 1, '}');
@@ -52,3 +115,4 @@ public class Eratosthenes {
 		System.out.println(str);
 	}
 }
+
