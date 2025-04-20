1f2f867028aa13eedbba28c5b0ea1faa1f6e0070
rolroralra <rolroralra@gmail.com>
2025-04-20 17:37:39 +0900

    docs: UnionFind

--- src/main/java/algorithm/UnionFind.java
+++ src/main/java/algorithm/UnionFind.java
@@ -2,10 +2,23 @@ package algorithm;
 
 import java.util.Arrays;
 
+/**
+ * Union-Find (Disjoint Set Union) data structure
+ * This class implements the Union-Find data structure with path compression and
+ * union by rank.
+ * It is used to efficiently manage and merge disjoint sets.
+ * <br/><br/>
+ * Valid index is from 0 to size - 1.
+ */
 public class UnionFind {
 	private final int[] parent;
 	private final int[] rank; // level or depth
 
+	/**
+	 * Constructor to initialize the Union-Find structure with a given size.
+	 *
+	 * @param size the number of elements in the Union-Find structure
+	 */
 	public UnionFind(int size) {
 		parent = new int[size];
 		rank = new int[size];
@@ -13,6 +26,12 @@ public class UnionFind {
 		Arrays.fill(rank, 1);
 	}
 
+	/**
+	 * Union operation to merge two teams containing each index.
+	 *
+	 * @param leftIndex index1
+	 * @param rightIndex index2
+	 */
 	public void union(int leftIndex, int rightIndex) {
 		int leftRootIndex = find(leftIndex);
 		int rightRootIndex = find(rightIndex);
@@ -31,7 +50,11 @@ public class UnionFind {
 		parent[rightRootIndex] = leftRootIndex;
 	}
 
-	@Deprecated
+	/**
+	 * @deprecated replace union() with unionByRank()
+	 * @see #union(int, int)
+	 */
+	@SuppressWarnings("unused")
 	private void unionByRank(int leftIndex, int rightIndex) {
 		int leftRootIndex = find(leftIndex);
 		int rightRootIndex = find(rightIndex);
@@ -54,12 +77,24 @@ public class UnionFind {
 		}
 	}
 
-
-
+	/**
+	 * Find operation to get the root index of the team containing the given index.
+	 *
+	 * @param index the index to find
+	 * @return the root index of the team containing the given index
+	 */
 	public int find(int index) {
 		return findByRecursive(index);
 	}
 
+	/**
+	 * Find operation to get the root index of the team containing the given index.
+	 * This method uses path compression to optimize the find operation.
+	 * This method is implemented using recursive approach.
+	 *
+	 * @param index the index to find
+	 * @return the root index of the team containing the given index
+	 */
 	private int findByRecursive(int index) {
 		if (parent[index] < 0) {
 			return index;
@@ -68,6 +103,15 @@ public class UnionFind {
 		return parent[index] = find(parent[index]);
 	}
 
+	/**
+	 * Find operation to get the root index of the team containing the given index.
+	 * This method uses path compression to optimize the find operation.
+	 * This method is implemented using iterative approach.
+	 *
+	 * @param index the index to find
+	 * @return the root index of the team containing the given index
+	 */
+	@SuppressWarnings("unused")
 	private int findByLoop(int index) {
 		int root = index;
 		while (parent[root] >= 0) {
---
+++ src/test/java/algorithm/UnionFindTest.java
@@ -0,0 +1,47 @@
+package algorithm;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import java.util.stream.IntStream;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class UnionFindTest {
+
+    private UnionFind unionFind;
+    private static final int TEST_SIZE = 10;
+
+    @BeforeEach
+    void setUp() {
+        unionFind = new UnionFind(TEST_SIZE);
+
+        IntStream.range(0, TEST_SIZE).forEach(i -> assertThat(unionFind.find(i)).isEqualTo(i));
+    }
+
+    @DisplayName("UnionFind::find() 테스트")
+    @Test
+    void testUnionFind() {
+        unionFind.union(0, 1);
+        assertThat(unionFind.find(0)).isEqualTo(unionFind.find(1));
+
+
+        unionFind.union(2, 3);
+        assertThat(unionFind.find(2)).isEqualTo(unionFind.find(3));
+
+        unionFind.union(1, 2);
+        assertAll(
+            () -> assertThat(unionFind.find(0)).isEqualTo(unionFind.find(1)),
+            () -> assertThat(unionFind.find(1)).isEqualTo(unionFind.find(0)),
+            () -> assertThat(unionFind.find(2)).isEqualTo(unionFind.find(0)),
+            () -> assertThat(unionFind.find(3)).isEqualTo(unionFind.find(0)),
+            () -> assertThat(unionFind.find(4)).isEqualTo(4),
+            () -> assertThat(unionFind.find(5)).isEqualTo(5),
+            () -> assertThat(unionFind.find(6)).isEqualTo(6),
+            () -> assertThat(unionFind.find(7)).isEqualTo(7),
+            () -> assertThat(unionFind.find(8)).isEqualTo(8),
+            () -> assertThat(unionFind.find(9)).isEqualTo(9)
+        );
+    }
+}
